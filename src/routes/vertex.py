dd33c978fe6def2a4af850b46f21d48383270a49
imrooteodoro <adelsonteodoro67@gmail.com>
2024-06-19 08:46:02 -0300

    feat: removendo o uso de funções prontas do python e definindo o  controller

--- src/routes/vertex.py
+++ src/routes/vertex.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, jsonify, render_template
-from models.city import *
-from models.graph import *
+from controllers.graph import Graph
 
-
+mapa_tocatins = Graph()
 def vertex(app):
     @app.route('/add_vertex', methods=['POST'])
     def add_vertex():
         data = request.get_json()
         city = data['city']
         info = data['info']
-        city_info[city] = info
+        mapa_tocatins.add_vertex(city, info)
         return jsonify({'message': 'Vértice adicionado com sucesso'}), 200
