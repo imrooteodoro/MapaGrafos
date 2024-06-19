4de01f0d8e95b01ff6ab7eeeb822edbdbcfbaf15
imrooteodoro <adelsonteodoro67@gmail.com>
2024-06-19 08:42:02 -0300

    feat: removendo o uso de bibliotecas e adaptando a rotas para usarem a classe Graph com controller

--- src/routes/index.py
+++ src/routes/index.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify, render_template
-from models.graph import *
-from models.city import *
+from controllers.graph import Graph
+from controllers.graph import tocantins_graph
+
+mapa_tocatins = Graph()
 
 def index_route(app):
     @app.route('/', methods=['GET'])
     def home():
-        print(graph)
-        return render_template('index.html', city_info = city_info, graph=graph)
+        print(mapa_tocatins.vertices)
+        return render_template('index.html', city_info = tocantins_graph.vertices, graph=tocantins_graph.edges)
