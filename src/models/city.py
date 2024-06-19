f531d25234639ee8ca9850719538870f5812aee4
imrooteodoro <adelsonteodoro67@gmail.com>
2024-06-19 08:36:22 -0300

    feat: removendo uso de função pronta

--- src/models/city.py
+++
@@ -1,8 +0,0 @@
-
-
-city_info = {
-    "Palmas": {"sigla": "TO", "populacao": 306296, "extensao_territorial": 2218.93},
-    "Araguaina": {"sigla": "TO", "populacao": 183779, "extensao_territorial": 4000.40},
-    "Gurupi": {"sigla": "TO", "populacao": 86413, "extensao_territorial": 1835.89}
-    
-    }
