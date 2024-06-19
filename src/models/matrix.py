e7d4e603abba1b93a69906ce10b10c3601b7d720
imrooteodoro <adelsonteodoro67@gmail.com>
2024-06-19 08:39:30 -0300

    feat: definindo models instanciando tocantins.graph

---
+++ src/models/matrix.py
@@ -0,0 +1,10 @@
+from controllers.graph import Graph
+from controllers.graph import tocantins_graph
+
+#
+matrix = {
+tocantins_graph.add_vertex("Palmas", {"sigla": "PAL", "populacao": 306296, "extensao_territorial": 2218.93}),
+tocantins_graph.add_edge("Araguaína", "Palmas", "BR - 153 - 450 KM"),
+tocantins_graph.add_vertex("Araguaína",{"sigla": "AUX", "população": 300000, "extensao_territorial": 2242.4})
+
+}
