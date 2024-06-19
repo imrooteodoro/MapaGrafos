217e546acb60b5b57ac2fa2d1c97dc066cb2d87c
imrooteodoro <adelsonteodoro67@gmail.com>
2024-06-19 08:44:01 -0300

    feat: removendo o uso de bibliotecas prontas e definindo os controllers

--- src/routes/rm_edge.py
+++ src/routes/rm_edge.py
@@ -1,13 +1,12 @@
-from models.graph import *
-from models.city import *
+from controllers.graph import Graph
 from flask import jsonify, request
 
+mapa_tocatins = Graph()
 def rm_edge(app):
     @app.route('/remove_edge', methods=['DELETE'])
     def remove_edge():
         data = request.get_json()
         city1 = data['city1']
         city2 = data['city2']
-        graph[city1] = [(v, info) for v, info in graph[city1] if v != city2]
-        graph[city2] = [(v, info) for v, info in graph[city2] if v != city1]
+        mapa_tocatins.remove_edge(city1, city2)
         return jsonify({'message': 'Aresta removida com sucesso'}), 200
